74dcb9a1060bcfb05001e0a8efb9a7784583260f
rebecavg <rebecavgallo@hotmail.com>
2024-01-25 16:57:42 -0500

    fixed functions

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 function fahrenheitToCelsius(degreesFahrenheit) {
     // The equation to convert from fahrenheit to celsius is
     //  (<degrees in fahrenheit> - 32) * 5/9 = <degrees in celsius>
-    let degreesCelsius;
 
-    (degreesFahrenheit - 32) * 5/9;
-
-    return degreesFahrenheit;
+    return (degreesFahrenheit - 32) * 5/9 
 }
 
 function celsiusToFahrenheit(degreesCelsius) {
     // The equation to convert from celsius to fahrenheit is
     //  (<degrees in celsius> * 9/5) + 32 = <degrees in fahrenheit>
-    let degreesFahrenheit = (degreesCelsius * 9/5) + 32;
 
-    return degreesFarenheit;
+    return (degreesCelsius * 9/5) + 32;
 }
 
 function getFirstStringFromArray(list) {
     // if list doesn't have a length property or it is 0 then it either isn't an array or
     // it is an empty array. In either case, return null.
-    if (!list.length) {
+    if (!list || list.length === 0) {
         return null;
     }
 
